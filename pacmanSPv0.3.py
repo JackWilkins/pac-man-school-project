49e09c4a8565558c70f2eba8ddad5bdb5e402d68
JackWilkins <31279910+JackWilkins@users.noreply.github.com>
2018-08-30 00:00:15 +0100

    movement/points
    added movement for the player sprite and also collision between the point sprites and the player sprite and the number of points goes up when a point is collected by the player

--- pacmanSPv0.3.py
+++ pacmanSPv0.3.py
@@ -1,6 +1,5 @@
 import pygame
 import os, sys
-from pygame.locals import *
 
 BLUE = (0,255,0)
 RED = (255,0,0)
@@ -15,32 +14,11 @@ class MainGame:
 
     def GameLoop(self):
         self.LoadSprites();
-        pygame.key.set_repeat(500, 30)
-        self.background = pygame.Surface(self.screen.get_size())
-        self.background = self.background.convert()
-        self.background.fill((255,255,255))
-        
+
         while 1:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-                elif event.type == KEYDOWN:
-                    if ((event.key == K_RIGHT)
-                    or (event.key == K_LEFT)
-                    or (event.key == K_UP)
-                    or (event.key == K_DOWN)):
-                        self.player.move(event.key)
-
-            Collide1 = pygame.sprite.spritecollide(self.player,self.point_sprites,True)
-            self.player.points = self.player.points + len(Collide1)
-            
-            self.screen.blit(self.background, (0, 0))
-            if pygame.font:
-                font = pygame.font.Font(None,36)
-                text = font.render("Points %s" % self.player.points,1,(0,0,255))
-                textpos = text.get_rect(centerx=self.width/2)
-                self.screen.blit(text,textpos)
-            
 
             self.point_sprites.draw(self.screen)
             self.player_sprites.draw(self.screen)
@@ -68,22 +46,6 @@ class Player(pygame.sprite.Sprite):
         self.image.fill(BLUE)
         self.rect = self.image.get_rect()
         self.points = 0
-        self.x_dist = 8
-        self.y_dist = 8
-
-    def move(self, key):
-        xMove = 0;
-        yMove = 0;
-
-        if (key == K_RIGHT):
-            xMove = self.x_dist
-        elif (key == K_LEFT):
-            xMove = -self.x_dist
-        elif (key == K_UP):
-            yMove = -self.y_dist
-        elif (key == K_DOWN):
-            yMove = self.y_dist
-        self.rect.move_ip(xMove,yMove);
 
 class Point(pygame.sprite.Sprite):
 
@@ -94,6 +56,7 @@ class Point(pygame.sprite.Sprite):
         if rect != None:
             self.rect = rect
         
+
 
 if __name__ == "__main__":
     MainWindow = MainGame()
