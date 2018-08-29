26f63400c140a9b85e027593d326db4dc1d31d46
JackWilkins <31279910+JackWilkins@users.noreply.github.com>
2018-08-29 23:20:13 +0100

    Points
    added point sprites and loaded them

--- pacmanSPv0.2.py
+++ pacmanSPv0.2.py
@@ -2,7 +2,6 @@ import pygame
 import os, sys
 
 BLUE = (0,255,0)
-RED = (255,0,0)
 
 class MainGame:
 
@@ -20,22 +19,12 @@ class MainGame:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            self.point_sprites.draw(self.screen)
             self.player_sprites.draw(self.screen)
             pygame.display.flip()
 
     def LoadSprites(self):
         self.player = Player()
         self.player_sprites = pygame.sprite.RenderPlain((self.player))
-
-        HorizontalP = int(self.width/64)
-        VerticalP = int(self.height/64)
-
-        self.point_sprites = pygame.sprite.Group()
-
-        for x in range(HorizontalP):
-            for y in range(VerticalP):
-                self.point_sprites.add(Point(pygame.Rect(x*64,y*64,64,64)))
 
 
 class Player(pygame.sprite.Sprite):
@@ -47,20 +36,10 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.points = 0
 
-class Point(pygame.sprite.Sprite):
-
-    def __init__(self,rect = None):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface([10,10])
-        self.image.fill(RED)
-        if rect != None:
-            self.rect = rect
-        
-
 
 if __name__ == "__main__":
     MainWindow = MainGame()
-    MainWindow.GameLoop()      
+    MainWindow.GameLoop()        
 
 
     
